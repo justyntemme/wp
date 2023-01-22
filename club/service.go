e1ff6f847cfa700040aacf0a54f73efa44c7ee0a
Justyn Temme <justyntemme@gmail.com>
2023-01-22 11:47:58 -0600

    Add club service

---
+++ club/service.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"context"
+
+	dal "github.com/justyntemme/wp/dal"
+)
+
+// @microgen middleware, logging, grpc, http, recovering
+// @protobuf github.com/justyntemme/wp/club/proto
+type ClubService interface {
+	// Add your methods here
+	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+	GetClubById(ctx context.Context, id string) (result string, err error)
+}
+
+func GetClubById(ctx context.Context, id string) (result string, err error) {
+
+	//var pipeline []bson.M
+
+	// Define the aggregation pipeline
+	// pipeline = []bson.M{
+	// 	{"$match": bson.M{"Id": "A"}},
+	// 	// {"$group": bson.M{"_id": "$cust_id", "total": bson.M{"$sum": "$amount"}}},
+	// 	// {"$sort": bson.M{"total": -1}},
+	// }
+	//
+	dal.Query(ctx, nil)
+
+	return "GetClubByID function has been called", nil
+}

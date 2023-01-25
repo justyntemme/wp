5091573c1420b9f9a091a0f7da8c395ab25f5163
Justyn Temme <justyntemme@gmail.com>
2023-01-24 21:08:18 -0600

    initial function for dal layer club-svc side

---
+++ club/database.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func init() {
+	// Connect to MongoDB
+	var err error
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://jupiter:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func query(ctx context.Context, pipeline []bson.D) (result string, err error) {
+
+	// Get a handle to the orders collection
+	orders := client.Database("wp").Collection("users")
+
+	// Execute the aggregation pipeline
+	cursor, err := orders.Aggregate(context.TODO(), pipeline)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Iterate through the result set
+	for cursor.Next(context.TODO()) {
+		var result bson.M
+		err := cursor.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(result)
+	}
+	return result, err
+}
+
+func GetClubsById(ctx context.Context, id string) (result string, err error) {
+
+	matchStage := bson.D{
+		{Key: "$match", Value: bson.D{
+			{Key: "cuid", Value: id},
+		},
+		},
+	}
+	projectStage := bson.D{
+		{Key: "$project", Value: bson.D{
+			{Key: "cuid", Value: 1},
+		}},
+	}
+	// pipeline = append(pipeline, bson.D{{"$match", bson.D{{"uuid", bson.D{{"$in", []string{id}}}}, bson.A{}}}})
+	// match := bson.D{
+	// 	"$match",
+	// 	bson.E{"uuid", id}}
+	var pipeline []bson.D
+
+	pipeline = append(pipeline, matchStage, projectStage)
+
+	return query(ctx, pipeline)
+
+}
--- club/go.mod
+++ club/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-kit/kit v0.8.0
 	github.com/gorilla/mux v1.7.3
 	go-micro.dev/v4 v4.9.0
+	go.mongodb.org/mongo-driver v1.11.1
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	google.golang.org/grpc v1.38.0
@@ -21,15 +22,22 @@ require (
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.0.4 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/kr/logfmt v0.0.0-20140226030751-b84e30acd515 // indirect
 	github.com/miekg/dns v1.1.43 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
